a9c66c603793cd18ec9ad15f1101651838120541
Jitendra bharati <jackjitendra768@gmail.com>
2021-09-08 18:40:18 +0530

    first commit

---
+++ enroll/forms.py
@@ -0,0 +1,19 @@
+from django.core import validators
+from django import forms
+from django.db import models
+from django.db.models import fields
+from django.forms import widgets 
+from .models import User
+
+
+class  StudentRegistration(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['name', 'email', 'password']
+
+        widgets = {
+            'name':forms.TextInput(attrs={'class': 'form-control'}),
+            'email':forms.EmailInput(attrs={'class': 'form-control'}),
+            'password':forms.PasswordInput(render_value= True,  attrs={'class': 'form-control'}),
+
+        }
